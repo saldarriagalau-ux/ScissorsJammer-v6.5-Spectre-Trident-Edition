dd7ef14933eebbe332122d10a636357e41746eb8
Scissors <saldarriaga.lau@gmail.com>
2026-06-23 11:40:32 -0300

    Update ScissorsJammer_v6.5_Spectre_Trident.ino

--- ScissorsJammer_v6.5_Spectre_Trident.ino
+++ ScissorsJammer_v6.5_Spectre_Trident.ino
@@ -5,7 +5,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <esp_wifi.h> // 🚀 LIBRERÍA NATIVA: Permite controlar la potencia de transmisión del Wi-Fi
+#include <esp_wifi.h> 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
